500546cddb3fcf0811c9444ab2babe46358c8b58
NovaBankz <45081335+NovaBankz@users.noreply.github.com>
2019-11-12 12:35:18 +0700

    Add soft edge flashlight effect

--- LearnOpenGLin10days/LearnOpenGLin10days/shader.fs
+++ LearnOpenGLin10days/LearnOpenGLin10days/shader.fs
@@ -2,34 +2,39 @@
 
 struct Material
 {
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
+	sampler2D diffuse;
+	sampler2D specular;
 	float shininess;
 };
 
 struct Light
 {
 	vec3 position;
+
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 direction;
+	float cutoff;
+	float outercutoff;
 };
 
 out vec4 FragColor;
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexCoord;
 
-uniform vec3 lightPos;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
 uniform vec3 viewPos;
 uniform Material material;
 uniform Light light;
 
 // some previous texture(ignored)
-in vec2 TexCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform float smiley_intensity;
@@ -37,26 +42,39 @@ uniform float smiley_intensity;
 void main()
 {
 	// FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(TexCoord.x, TexCoord.y)), smiley_intensity);
-	// vec4 textureMap = texture(texture2, TexCoord);
+	// vec4 textureMap = texture(texture2, TexCoord
+
 
 	// Phong Shading	1. ambient, 2. diffuse, 3.specular
 	// ambient
-	vec3 ambient = material.ambient * light.ambient;
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoord));
 
 	// diffuse
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(lightPos - FragPos);
+	vec3 lightDir = normalize(light.position - FragPos);
 	float diffStrength = max(dot(norm, lightDir), 0.0f);
-	vec3 diffuse = diffStrength * material.diffuse * light.diffuse;
+	vec3 diffuse = diffStrength * light.diffuse * vec3(texture(material.diffuse, TexCoord));
 
 	// specular
 	vec3 viewDir =	normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
-	vec3 specular = spec * material.specular * light.specular;
+	vec3 specular = spec * light.specular * vec3(texture(material.specular, TexCoord));
 
+	// additional effects
+	float distance = length(FragPos - light.position);
+	float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+
+	float theta = dot(lightDir, normalize(-light.direction));
+	float epsilon = light.cutoff - light.outercutoff;
+	float intensity = (theta - light.outercutoff)/ epsilon;
+
+	diffuse *= attenuation;
+	specular *= attenuation;
+	diffuse *= intensity;
+	specular *= intensity;
 	// result
-	vec3 result = (ambient + diffuse + specular) * objectColor;
+	vec3 result = (ambient + diffuse + specular);
 
 	FragColor = vec4(result, 1.0f);
 }
--- LearnOpenGLin10days/LearnOpenGLin10days/shader.vs
+++ LearnOpenGLin10days/LearnOpenGLin10days/shader.vs
@@ -1,24 +1,10 @@
 #version 330 core
 
-struct Material
-{
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
-	float shininess;
-};
-
-struct Light
-{
-	vec3 position;
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
-};
-
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aTexCoord;
 
+out vec2 TexCoord;
 out vec3 Normal;
 out vec3 FragPos;
 out vec3 result;
@@ -27,35 +13,12 @@ uniform mat4 modelMat;
 uniform mat4 viewMat;
 uniform mat4 projectMat;
 
-uniform vec3 lightPos;
-uniform vec3 lightColor;
-uniform vec3 objectColor;
-uniform vec3 viewPos;
-uniform Material material;
-uniform Light light;
-
 void main()
 {
-	// ambient
-	vec3 ambient = material.ambient * light.ambient;
-
-	// diffuse
-	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(lightPos - FragPos);
-	float diffStrength = max(dot(norm, lightDir), 0.0f);
-	vec3 diffuse = diffStrength * material.diffuse * light.diffuse;
-
-	// specular
-	vec3 viewDir =	normalize(viewPos - FragPos);
-	vec3 reflectDir = reflect(-lightDir, norm);
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0f), material.shininess);
-	vec3 specular = spec * material.specular * light.specular;
-
 	gl_Position = projectMat * viewMat * modelMat * vec4(aPos, 1.0f);
 
 	// in global coordinate
 	FragPos = vec3(modelMat * vec4(aPos, 1.0f));
 	Normal = mat3(transpose(inverse(modelMat))) * aNormal;
-	result = ambient + diffuse + specular;
-
+	TexCoord = aTexCoord;
 }
